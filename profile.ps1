893fb87152c51ac7592f41e4fa7400375f7c526b
Bill Gan <billgan.pblpbl@gmail.com>
2024-01-25 10:02:28 -0500

    .

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,16 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_lean.omp.json" | Invoke-Expression
 Set-PSReadLineOption -PredictionSource None
 Import-Module posh-git
+
+# git-update for "git add ., git commit -m ., git push"
+function gupd {
+    git add .
+    git commit -m "."
+    git push
+}
+
+# function gcmsg {
+#     git commit -m "."
+# }
 # & "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1" -SkipAutomaticLocation
 # cd code
