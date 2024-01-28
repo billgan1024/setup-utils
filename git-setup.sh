123f6ff4014b6b066dce857f28343069eaa56260
Bill Gan <billgan.pblpbl@gmail.com>
2024-01-28 10:57:09 -0500

    .

--- git-setup.sh
+++ git-setup.sh
@@ -2,3 +2,4 @@ git config user.email "0xbgan@gmail.com"
 git config user.name "Bill Gan"
 git config push.autoSetupRemote true
 git config --global core.autocrlf false
+git config advice.addIgnoredFile false
